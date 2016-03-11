91d9ffa8e44bc6f7465b69b8014411bf31e36bd8
Viktor Smari <viktorsmari@gmail.com>
2016-03-11 20:40:06 +0100

    Login system ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   devise_for :users
   resources :usertypes
   root 'welcome#index'
@@ -7,7 +9,7 @@ Rails.application.routes.draw do
   resources :lectures
   resources :student_registrations
   resources :seminars
-  resources :users
+#  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
