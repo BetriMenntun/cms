544235b8b84f0a660ef4ca9f4162f3e1ab19b8bc
Viktor Smari <viktorsmari@gmail.com>
2016-03-13 10:29:08 +0100

    Frontpage dashboard shows your seminars

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
+  before_action :authenticate_user!
   def index
+    @teaching = StudentRegistration.where(:user_id => current_user.id)
+
+    @learning = StudentRegistration.where(:user_id => current_user.id)
   end
 end
